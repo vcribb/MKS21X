4a2403230be2f89a77fce637ff80a2425d28f75b
vcribb <vcribb@stuy.edu>
2017-11-28 15:25:42 -0500

    this is the reversedcharseq

---
+++ 04ReversedCharSeq/ReversedCharSeq.java
@@ -0,0 +1,25 @@
+public class ReversedCharSeq{
+    private String str;
+    public ReversedCharSeq(String s){
+        str = "";
+	for (int x = s.length()-1; x > -1; x--){
+	    str += s.substring(x,x+1);
+	}
+    }
+    public char charAt(int index){
+	return str.charAt(index);
+    }
+    public int length(){
+	return str.length();
+    }
+    public CharSequence subSequence(int start, int end){
+	String s = "";
+	for (int x = start; x < end; x++){
+	    s+=str.charAt(x);
+	}
+	return s;
+    }
+    public String toString(){
+	return str;
+    }
+}
